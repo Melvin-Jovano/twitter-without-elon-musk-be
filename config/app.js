ca4cfaa691080d6a7c6355a5d14bb9d43090b484
Melvin-Jovano <melvinjovano2@gmail.com>
2023-01-14 19:19:00 +0700

    feat: change schema

--- config/app.js
+++ config/app.js
@@ -2,7 +2,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-    APP_PORT: process.env.APP_PORT
+    APP_NAME: 'Twitter 2.0',
+    APP_PORT: process.env.APP_PORT,
+    ACCESS_TOKEN_SECRET: 'roses-are-red',
+    REFRESH_TOKEN_SECRET: 'violets-are-blue'
 }
 
 export default config;
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import config from '../config/app.js';
+import bycrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
@@ -6,7 +8,7 @@ export const login = async ( req, res ) => {
     try {
         const {username, password} = req.body;
 
-        // TODO Encrypt Password First
+        // TODO Check Password First
         const getUserByUsernameAndPassword = await prisma.user.findFirst({
             where: {
                 username,
@@ -16,13 +18,53 @@ export const login = async ( req, res ) => {
 
         if(getUserByUsernameAndPassword !== null) {
             res.send({
-                message: 'Hello'
+                message: 'Hello',
+                data: null
             });
             return;
         } 
 
         res.send({
-            message: 'Who tf Are You?'
+            message: 'Who tf Are You?',
+            data: null
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({message : 'Oops Error'});
+    }
+}
+
+export const signUp = async ( req, res ) => {
+    try {
+        const {username, password} = req.body;
+
+        // Check If Theres User With Given Username
+        const getUserByUsername = await prisma.user.findFirst({
+            where: {
+                username
+            }
+        });
+
+        // If No Then Good To Go
+        if(getUserByUsername === null) {
+            const hashedPassword = bycrypt.hashSync(password, 10);
+            await prisma.user.create({
+                data: {
+                    username,
+                    password: hashedPassword
+                }
+            });
+
+            res.send({
+                message: `Hello ${username}, Welcome To ${config.APP_NAME}`,
+                data: null
+            });
+            return;
+        } 
+
+        res.send({
+            message: 'Username Already Taken',
+            data: null
         });
     } catch (error) {
         console.error(error);
